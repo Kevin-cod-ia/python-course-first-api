2f0b9841e8b530648630544862f6928d143d8bfc
Kevin Neres <kevin.dionisio@yahoo.com.br>
2024-05-13 09:16:03 -0300

    second commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -12,7 +12,33 @@ purchase_orders = [
                 'id': 1,
                 'description': 'Item de compra 1',
                 'price': 20.99
-            }
+            }        
+        ],
+
+    },
+
+    {
+        'id': 2,
+        'description': 'Pedido 27',
+        'items': [
+            {
+                'id': 2,
+                'description': 'Item de compra 10',
+                'price': 50.99
+            }  
+        ],
+
+    },
+
+    {
+        'id': 3,
+        'description': 'Pedido 17',
+        'items': [
+            {
+                'id': 3,
+                'description': 'Item de compra 12',
+                'price': 10.99
+            }  
         ]
     }
 ]
@@ -26,5 +52,51 @@ def home():
 @app.route('/purchase_orders')
 def get_purchase_orders():
     return jsonify(purchase_orders)
+
+
+@app.route('/purchase_orders/<int:id>')
+def get_purchase_orders_by_id(id):
+    for order in purchase_orders:
+        if id == order['id']:
+            return jsonify(order)
+    return f'ERRO 404! Pedido não encontrado. ---'
+
+
+@app.route('/purchase_orders',methods=['POST'])
+def create_purchase_order():
+    request_data = request.get_json()
+
+    purchase_order = {
+        'id': request_data['id'],
+        'description': request_data['description'],
+        'items': []
+    }
+
+    purchase_orders.append(purchase_order)
+
+    return jsonify(purchase_order)
+
+
+@app.route('/purchase_orders/<int:id>/items')
+def get_purchase_items_by_id(id):
+    for order in purchase_orders:
+        if id == order['id']:
+            return jsonify(order['items'])
+    return f'ERRO 404! Pedido não encontrado. ---'
+
+
+@app.route('/purchase_orders/<int:id>/items', methods=['POST'])
+def create_purchase_items_by_id(id):
+    req_data = request.get_json()
+    for order in purchase_orders:
+        if id == order['id']:
+            order['items'].append({
+                'id': req_data['id'],
+                'description': req_data['description'],
+                'price': req_data['price']
+            })
+            return jsonify(order)
+    return f'ERRO 404! Pedido não encontrado. ---'
+
 
 app.run(port=5000)
